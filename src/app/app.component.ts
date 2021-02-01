970b5c25ff894101668388ad79e1b024036ccc4c
ae <ae@corp2.net>
2021-02-01 08:07:15 +0200

    splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+// import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    // private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    public router: Router,
   ) {
     this.initializeApp();
   }
@@ -21,7 +23,8 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.router.navigateByUrl('splash');
+      // this.splashScreen.hide();
     });
   }
 }
